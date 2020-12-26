b72132f2f1f48ea0b1a87b802e3d4e149797f1ad
MURA199918 <muralisaranr18@gmail.com>
2020-12-26 13:07:46 +0530

    [Murali]Refactored to calculate wage for number of working days

--- employeeWage.js
+++ employeeWage.js
@@ -1,29 +1,95 @@
 console.log("Welcome to employee Wage problem");
 
-const IS_PART_TIME = 1;
-const IS_FULL_TIME = 2;
-const PART_TIME_HOURS = 4;
-const FULL_TIME_HOURS = 8;
-const WAGE_PER_HOUR = 20;
-const NUM_OF_WORKING_DAYS = 2;
+//UC1 IF ELSE Condition
+{
+    const IS_ABSENT = 0;
+    let empCheck = Math.floor(Math.random() * 10) % 2;
+    if(empCheck == IS_ABSENT){
+          console.log("UC1- Employee is Absent");
+    }else{
+          console.log("UC1- Employee is PRESENT");
+   }
+}
 
+//UC2- SwitchCase
+{
+        const IS_PART_TIME = 1;
+        const IS_FULL_TIME = 2;
+        const PART_TIME_HOURS = 4;
+        const FULL_TIME_HOURS = 8;
+        const WAGE_PER_HOUR = 20;
 
-function getWorkingHours(empCheck){
-    switch(empCheck){
-        case IS_PART_TIME: 
+        let empHrs = 0;
+        let empCheck = Math.floor(Math.random() * 10) % 3;
+        switch(empCheck){
+            case IS_PART_TIME: 
+               empHrs = PART_TIME_HOURS;
+               break;
+            case IS_FULL_TIME:
+               empHrs = FULL_TIME_HOURS;
+               break;
+            default:
+               empHrs = 0;
+        }
+
+        let empWage = empHrs * WAGE_PER_HOUR;
+        console.log("UC2- EmpWage: "+empWage);
+}
+
+//UC3- Functions
+{
+        const IS_PART_TIME = 1;
+        const IS_FULL_TIME = 2;
+        const PART_TIME_HOURS = 4;
+        const FULL_TIME_HOURS = 8;
+        const WAGE_PER_HOUR = 20;
+
+        let empCheck = Math.floor(Math.random() * 10) % 3;
+
+        function getWorkingHours(empCheck){
+        switch(empCheck){
+                case IS_PART_TIME: 
                    return PART_TIME_HOURS;
-        case IS_FULL_TIME:
+                case IS_FULL_TIME:
                    return FULL_TIME_HOURS;
-        default:
+                default:
                    return 0;
-    }
+            }
+         }
+
+         let empHrs = 0;
+         empHrs = getWorkingHours(empCheck);
+         let empWage = empHrs * WAGE_PER_HOUR;
+         console.log("UC3- EmpWage: "+empWage);
 }
 
-let empHrs = 0;
-for(let day = 0; day < NUM_OF_WORKING_DAYS; day++){
-    let empCheck = Math.floor(Math.random() * 10) % 3;
-    empHrs += getWorkingHours(empCheck);
-}
+//UC4- For Loop
+{
+        const IS_PART_TIME = 1;
+        const IS_FULL_TIME = 2;
+        const PART_TIME_HOURS = 4;
+        const FULL_TIME_HOURS = 8;
+        const WAGE_PER_HOUR = 20;
+        const NUM_OF_WORKING_DAYS = 2;
 
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Total Hrs: "+empHrs+ " Emp Wage: "+empWage);
+
+        function getWorkingHours(empCheck){
+            switch(empCheck){
+                case IS_PART_TIME: 
+                   return PART_TIME_HOURS;
+                case IS_FULL_TIME:
+                   return FULL_TIME_HOURS;
+                default:
+                   return 0;
+            }
+        }
+
+        let empHrs = 0;
+        for(let day = 0; day < NUM_OF_WORKING_DAYS; day++){
+            let empCheck = Math.floor(Math.random() * 10) % 3;
+            empHrs += getWorkingHours(empCheck);
+        }
+
+        let empWage = empHrs * WAGE_PER_HOUR;
+        console.log("UC4- Total Hrs: "+empHrs+ " UC4- Emp Wage: "+empWage);
+}
