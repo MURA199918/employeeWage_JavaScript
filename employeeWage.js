0edead0ed03cec35c003197d0b24ae84f2ad43d4
MURA199918 <muralisaranr18@gmail.com>
2020-12-26 12:37:24 +0530

    [Murali]Refactored to check if employee is present or absent

--- employeeWage.js
+++ employeeWage.js
@@ -1,11 +1,13 @@
 console.log("Welcome to employee Wage problem");
 
-const IS_ABSENT = 0;
-
-let empCheck = Math.floor(Math.random() * 10) % 2;
-if(empCheck == IS_ABSENT){
-    console.log("Employee is Absent");
-    return;
-}else{
-    console.log("Employee is PRESENT");
+//UC1 IF ELSE Condition
+{
+        const IS_ABSENT = 0;
+        let empCheck = Math.floor(Math.random() * 10) % 2;
+        if(empCheck == IS_ABSENT){
+              console.log("UC1- Employee is Absent");
+              return;
+        }else{
+              console.log("UC1- Employee is PRESENT");
+       }
 }
