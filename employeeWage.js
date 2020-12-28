6f42eff96ed29d25f2184c068f85a1fd3aa5b8b8
MURA199918 <muralisaranr18@gmail.com>
2020-12-28 11:00:34 +0530

    [Murali]Refactored to store day and daily wage along with total wage using maps to store day wise wage

--- employeeWage.js
+++ employeeWage.js
@@ -263,3 +263,50 @@ console.log("Welcome to employee Wage problem");
     }
     console.log("UC7G - Number of days emp worked: "+empDailyWageArr.reduce(totalDaysWorked, 0));
 }
+
+//UC8 - Maps
+{
+    const IS_PART_TIME = 1;
+    const IS_FULL_TIME = 2;
+    const PART_TIME_HOURS = 4;
+    const FULL_TIME_HOURS = 8;
+    const WAGE_PER_HOUR = 20;
+    const NUM_OF_WORKING_DAYS = 20;
+    const MAX_HRS_IN_MONTH = 160;
+
+    let totalEmpHrs = 0;
+    let totalWorkingDays = 0;
+    let empDailyWageArr = new Array();
+    let empDailyWageMap = new Map();
+
+    function calcDailyWage(empHrs){
+        return empHrs * WAGE_PER_HOUR;
+    }
+
+    function getWorkingHours(empCheck){
+        switch(empCheck){
+            case IS_PART_TIME: 
+                return PART_TIME_HOURS;
+            case IS_FULL_TIME:
+                return FULL_TIME_HOURS;
+            default:
+                return 0;
+        }
+    }
+
+    while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+        totalWorkingDays++;
+        let empCheck = Math.floor(Math.random() * 10) % 3;
+        let empHrs = getWorkingHours(empCheck);
+        totalEmpHrs += empHrs;
+        empDailyWageArr.push(calcDailyWage(empHrs));
+        empDailyWageMap.set(totalWorkingDays, calcDailyWage(empHrs));
+    }
+
+    console.log(empDailyWageMap);
+    function totalWages(totalWage, dailyWage){
+        return totalWage + dailyWage;
+    }
+    console.log("UC8 - Emp Wage Map totalHrs: "+Array.from(empDailyWageMap.values()).reduce(totalWages, 0));
+
+}
