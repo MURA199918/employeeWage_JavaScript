e1786660566d3e39b21240017b946334b4c564d0
MURA199918 <muralisaranr18@gmail.com>
2020-12-28 10:24:57 +0530

    [Murali]Refactored to store daily wage along with total wage using arrays

--- employeeWage.js
+++ employeeWage.js
@@ -127,3 +127,44 @@ console.log("Welcome to employee Wage problem");
                 let empWage = totalempHrs * WAGE_PER_HOUR;
                 console.log("UC5-Total Days: "+totalWorkingDays+ " Total Hrs: "+totalempHrs+ " Emp Wage: "+empWage);
 }
+
+//UC6 - Arrays
+{
+    const IS_PART_TIME = 1;
+    const IS_FULL_TIME = 2;
+    const PART_TIME_HOURS = 4;
+    const FULL_TIME_HOURS = 8;
+    const WAGE_PER_HOUR = 20;
+    const NUM_OF_WORKING_DAYS = 20;
+    const MAX_HRS_IN_MONTH = 160;
+
+    let totalEmpHrs = 0;
+    let totalWorkingDays = 0;
+    let empDailyWageArr = new Array();
+
+    function calcDailyWage(empHrs){
+        return empHrs * WAGE_PER_HOUR;
+    }
+
+    function getWorkingHours(empCheck){
+        switch(empCheck){
+            case IS_PART_TIME: 
+                return PART_TIME_HOURS;
+            case IS_FULL_TIME:
+                return FULL_TIME_HOURS;
+            default:
+                return 0;
+        }
+    }
+
+    while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+        totalWorkingDays++;
+        let empCheck = Math.floor(Math.random() * 10) % 3;
+        let empHrs = getWorkingHours(empCheck);
+        totalEmpHrs += empHrs;
+        empDailyWageArr.push(calcDailyWage(empHrs));
+    }
+
+    let empWage = calcDailyWage(totalEmpHrs);
+    console.log("UC6 - Total Days: "+totalWorkingDays+" Total Hrs: "+totalEmpHrs+" Emp Wage: "+empWage);
+}
