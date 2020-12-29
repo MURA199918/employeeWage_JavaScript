5b4ebbede7789602627da87c7f48c068db6ce09c
MURA199918 <muralisaranr18@gmail.com>
2020-12-29 10:34:21 +0530

    [Murali]Refactored to create employee payroll class with id, name and salary

--- employeeWage.js
+++ employeeWage.js
@@ -490,3 +490,33 @@ console.log("Welcome to employee Wage problem");
                             .map(dailyHrsAndWage => dailyHrsAndWage.dayNum);
     console.log("UC11D - NonWorkingDayNums: "+nonWorkingDayNums);
 }
+
+//UC11 - Classes
+{
+    class EmployeePayrollData{
+        //property
+        id;
+        salary;
+
+        //constructor
+        constructor(id, name, salary){
+            this.id = id;
+            this.name = name;
+            this.salary = salary;
+        }
+
+        //getter and setter method
+        get name() { return this._name; }
+        set name(name) { this._name = name;}
+
+        //method
+        toString(){
+            return "id=" + this.id + ", name='" + this.name + ", salary=" + this.salary;
+        }
+    }
+
+    let employeePayrollData = new EmployeePayrollData(1, "Mark", 30000);
+    console.log(employeePayrollData.toString());
+    employeePayrollData.name = "john";
+    console.log(employeePayrollData.toString());
+}
