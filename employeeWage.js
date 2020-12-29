2b04fab7b82fe96aa15d8cdec64bd40a51c7814c
MURA199918 <muralisaranr18@gmail.com>
2020-12-29 09:54:35 +0530

    [Murali]Refactored to store day, hours worked and wage earned in a single object

--- employeeWage.js
+++ employeeWage.js
@@ -373,3 +373,51 @@ console.log("Welcome to employee Wage problem");
     console.log("Non working days: "+nonWorkingDays);
 }
 
+//UC10 - Object Creation
+{
+    const IS_PART_TIME = 1;
+    const IS_FULL_TIME = 2;
+    const PART_TIME_HOURS = 4;
+    const FULL_TIME_HOURS = 8;
+    const WAGE_PER_HOUR = 20;
+    const NUM_OF_WORKING_DAYS = 20;
+    const MAX_HRS_IN_MONTH = 160;
+
+    let totalEmpHrs = 0;
+    let totalWorkingDays = 0;
+    let empDailyHrsAndWageArr = new Array();
+
+    function calcDailyWage(empHrs){
+        return empHrs * WAGE_PER_HOUR;
+    }
+
+    function getWorkingHours(empCheck){
+        switch(empCheck){
+            case IS_PART_TIME: 
+                return PART_TIME_HOURS;
+            case IS_FULL_TIME:
+                return FULL_TIME_HOURS;
+            default:
+                return 0;
+        }
+    }
+
+    while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+        totalWorkingDays++;
+        let empCheck = Math.floor(Math.random() * 10) % 3;
+        let empHrs = getWorkingHours(empCheck);
+        totalEmpHrs += empHrs;
+        empDailyHrsAndWageArr.push(
+            {
+                dayNum:totalWorkingDays,
+                dailyHours:empHrs,
+                dailyWage: calcDailyWage(empHrs),
+                toString(){
+                    return '\nDay' + this.dayNum + ' => Working Hours is ' + this.dailyHours +
+                           ' And Wage Earned = ' + this.dailyWage
+                },
+            }
+        );
+    }
+    console.log("UC10 - Showing daily hours worked and wage earned: "+empDailyHrsAndWageArr);
+}
